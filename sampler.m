4388b04665bc04f322a675f01a3cceee44d8dcd3
josh <joshualilly@me.com>
2018-03-18 20:08:30 -0400

    sampler updates

--- sampler.m
+++ sampler.m
@@ -1,16 +1,49 @@
-[y,fs] = audioread('recording.aifc');
-ydft = fft(y);
-% y has even length
-ydft = ydft(1:length(y)/2+1);
+Fs = 44100;
 
-% create a frequency vector
-freq = 0:fs/length(y):fs/2;
+microphone = dsp.AudioRecorder('SamplesPerFrame', samplesPerFrame, 'NumChannels', 1);
 
-% plot magnitude
-subplot(211);
-plot(freq,abs(ydft));
+spectrum = dsp.SpectrumAnalyzer('SampleRate', Fs);
 
-% plot phase
-subplot(212);
-plot(freq,unwrap(angle(ydft))); 
-xlabel('Hz');
+window = 1764;            % stft sample in hamming window
+noverlap = 882;           % the overlap of the stft samples
+nfft = 44100;             % number of fft samples
+
+HiLo=[18000 22000];       % frequency bounds
+nHiLo=round(HiLo);
+
+tic;
+while toc < 50
+    % get a sample from the microphone
+    audioIn = step(microphone);
+    
+    % do stft with the window number overlapped nfft and the sampling freq
+    [s, F, T, P] = spectrogram(audioIn, window, noverlap, nfft, Fs);
+    
+    % get the number of samples
+    Nsamps = length(audioIn);
+    
+    % calculate the frequencies
+    freq = Fs*(0:Nsamps/2-1)/Nsamps;
+    
+    % only want the real portions of the stft
+    sAbs =  abs(s);
+    
+    % find the max power frequency in the window 18000 - 22000
+    [maxValue, indexMax] = max(sAbs(nHiLo(1):nHiLo(2)));
+    
+    % index is max index + the window start
+    indexMax = indexMax + nHiLo(1);
+
+    % display the strongest frequency
+    disp(freq(indexMax));
+
+    % paint the spectorgram for debugging
+    spectrogram(audioIn,[], [], [], Fs, 'yaxis')
+    
+    % paint the spectrum analyzer
+    step(spectrum, audioIn);
+end
+
+% clean up
+release(microphone)
+release(spectrum)
