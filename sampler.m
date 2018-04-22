cdda25ac97425d03dfe00064b66c12cd243ac2b9
josh <joshualilly@me.com>
2018-04-22 14:55:34 -0400

    Sampler outlier removal

--- sampler.m
+++ sampler.m
@@ -29,27 +29,35 @@ range5 = round(twentyEightRange);
 
 % calculate the frequencies
 freq = Fs*(0:samplesPerFrame/2-1)/samplesPerFrame;
+freq1 = 20000;
+freq2 = 20200;
+freq3 = 20400;
+freq4 = 20600;
+freq5 = 20800;
 freq1Avg = (20000 + 20200 + 20400 + 20600 + 20800) / 5;
 
 values=0:1/fs:duration;
 
 xdist = 0;
 counter = 0;
+lastDopplerShift = 0;
 tic;
-while toc < 50
+while toc < 2
     % get a sample from the microphone
     audioIn = step(microphone);
     
     % do stft with the window number overlapped nfft and the sampling freq
-    [s, F, T, P] = spectrogram(audioIn, hamming(window), noverlap, nfft, Fs);
+    [s, K, Q, P] = spectrogram(audioIn, hamming(window), noverlap, nfft, Fs);
   
+    % find the noise variance for each channel which is the average of all
+    % of the signal not near the base signal
     noisevar1 = (sum(P(19050:19990))/40) + (sum(P(20010:20050))/40);
     noisevar2 = (sum(P(20150:20190))/40) + (sum(P(20210:20250))/40);
     noisevar3 = (sum(P(20350:20390))/40) + (sum(P(20410:20450))/40);
     noisevar4 = (sum(P(20550:20590))/40) + (sum(P(20610:20650))/40);
     noisevar5 = (sum(P(20750:20790))/40) + (sum(P(20810:20850))/40);
 
-    % remove the noise
+    % weight each frame against inverse of the noise variance for MRC
     s(range1(1):range1(2)) = s(range1(1):range1(2)) / noisevar1;
     s(range2(1):range2(2)) = s(range2(1):range2(2)) / noisevar2;
     s(range3(1):range3(2)) = s(range3(1):range3(2)) / noisevar3;
@@ -73,6 +81,8 @@ while toc < 50
     indexMax4 = indexMax4 + range4(1);
     indexMax5 = indexMax5 + range5(1);
 
+    % each signal has been weighted against inverse of noise variance so
+    % average
     avg = (freq(indexMax1) + freq(indexMax2) + freq(indexMax3) + freq(indexMax4) + freq(indexMax5)) / 5;
     
     % display the strongest frequency
@@ -83,18 +93,43 @@ while toc < 50
     fprintf('freq5: %i\n', freq(indexMax5));
     
     dopplerShift = freq1Avg - avg;
+    
+    % outlier removal
+    if (dopplerShift > 10)
+        % if the shift is larger than 10 then take the closest to the last
+        % doppler shift estimation
+        a = [(freq1 - freq(indexMax1))
+             (freq2 - freq(indexMax2))
+             (freq3 - freq(indexMax3)) 
+             (freq4 - freq(indexMax4))
+             (freq5 - freq(indexMax5))];
+         
+        b = [abs(lastDopplerShift - abs(a(1)))
+             abs(lastDopplerShift - abs(a(2)))
+             abs(lastDopplerShift - abs(a(3)))
+             abs(lastDopplerShift - abs(a(4)))
+             abs(lastDopplerShift - abs(a(5)))];
+         
+        [value, index] = min(b);
+        % assign doppler shift to the closest
+        dopplerShift = a(index);
+    end
+    lastDopplerShift = dopplerShift;
+    
+    %[kamf K M F] = kalman(dopplerShift, 0.00001, 0.00001);
+    %kalmf = kalmf(1,:)
     fprintf('avg %i\n', avg);
     
     fprintf('Doppler shift %i\n', dopplerShift);
     
-    xdist = xdist + (avg * 340)* 0.4;
+    xdist = xdist + (avg)* 0.4;
     
     fprintf('-----------------------\n');
-    fprintf('x dist %i\n', xdist);
+    fprintf('x dist %i\n', xdist/1000);
     fprintf('-----------------------\n');
     
     % paint the spectorgram for debugging
-    spectrogram(audioIn, window, noverlap, nfft, Fs, 'yaxis')
+    %spectrogram(audioIn, window, noverlap, nfft, Fs, 'yaxis')
     
     % paint the spectrum analyzer
     step(spectrum, audioIn);
